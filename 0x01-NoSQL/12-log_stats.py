f00b2599b513138f27a3b757c3f4befa5201f82b
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-10-22 13:06:04 +0100

    you

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,14 +5,13 @@ from pymongo import MongoClient
 
 def count_logs(nginx_col):
     """Provides some stats about Nginx logs"""
-
     count = nginx_col.estimated_document_count()
     print(f'{count} logs')
     print('Methods:')
 
     for stat in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
         count = nginx_col.count_documents({"method": stat})
-        print(f'\tmethod {stat}: {count}')
+        print(f'    method {stat}: {count}')
 
     count = nginx_col.count_documents({"method": "GET", "path": "/status"})
     print(f'{count} status check')
