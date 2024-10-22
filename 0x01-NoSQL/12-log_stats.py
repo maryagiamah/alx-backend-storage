9af934034d6bea95fe6d3415325fe749587663ab
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-10-22 12:40:44 +0100

    you

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,10 +2,9 @@
 """Write a Python script that provides some stats about Nginx logs"""
 from pymongo import MongoClient
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_col = client.logs.nginx
-    
+
+def count_logs(nginx_col):
+    """Provides some stats about Nginx logs"""
     count = nginx_col.count_documents({})
     print(f'{count} logs')
     print('Methods:')
@@ -16,3 +15,8 @@ if __name__ == "__main__":
 
     count = nginx_col.count_documents({"method": "GET", "path": "/status"})
     print(f'{count} status check')
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+
+    count_logs(client.logs.nginx)
