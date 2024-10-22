2e5093b5d5c363ee8afba829fe9f0d6d499ed166
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-10-22 12:50:02 +0100

    you

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,16 +3,21 @@
 from pymongo import MongoClient
 
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-nginx_col = client.logs.nginx
+def count_logs(nginx_col):
+    """Provides some stats about Nginx logs"""
 
-count = nginx_col.estimated_document_count()
-print(f'{count} logs')
-print('Methods:')
+    count = nginx_col.estimated_document_count()
+    print(f'{count} logs')
+    print('Methods:')
 
-for stat in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-    count = nginx_col.count_documents({"method": stat})
-    print(f'    method {stat}: {count}')
+    for stat in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        count = nginx_col.count_documents({"method": stat})
+        print(f'    method {stat}: {count}')
 
-count = nginx_col.count_documents({"method": "GET", "path": "/status"})
-print(f'{count} status check')
+    count = nginx_col.count_documents({"method": "GET", "path": "/status"})
+    print(f'{count} status check')
+
+
+if __name__ == '__main__':
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    count_logs(client.logs.nginx)
