56c4d810673dea29a3723c92681ad8f237d403e0
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-10-23 14:32:28 +0100

    you

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Create a Cache class"""
+import redis
+import uuid
+from typing import Any
+
+
+class Cache:
+    """Cache class"""
+
+    def __init__(self):
+        """Init Class"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Any) -> int:
+        """Store method"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
