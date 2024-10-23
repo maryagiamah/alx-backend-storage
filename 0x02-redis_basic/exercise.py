4950d19646b7d9bdd5261742f5bd89fecc7405d7
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-10-23 14:41:17 +0100

    you

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Create a Cache class"""
 import redis
 import uuid
-from typing import Any
+from typing import Union
 
 
 class Cache:
@@ -13,7 +13,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Any) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store method"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
