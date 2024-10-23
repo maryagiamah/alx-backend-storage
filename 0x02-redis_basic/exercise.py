64bd4222c1ad2f06b58e9a4ec1f765899872e66f
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-10-23 16:43:40 +0100

    you

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,6 +18,21 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """Store history of imput and output"""
+    in_list = f"{method.__qualname__}:inputs"
+    out_list = f"{method.__qualname__}:outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Returned Callable"""
+        self._redis.rpush(in_list, str(args))
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(out_list, output)
+        return output
+    return wrapper
+
+
 class Cache:
     """Cache class"""
 
@@ -27,6 +42,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store method"""
         key = str(uuid.uuid4())
