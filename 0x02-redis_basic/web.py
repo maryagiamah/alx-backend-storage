d313b553f39e9d4c744ac956bb38838116f9c83e
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-10-24 12:57:55 +0100

    you

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -17,17 +17,14 @@ def count_calls(method: Callable) -> Callable:
         count_key = f"count:{url}"
         cache_key = f"cache:{url}"
 
-        # Increment the count for this URL
         red_loc.incr(count_key)
 
-        # Check if the URL is cached
         cache_res = red_loc.get(cache_key)
         if cache_res:
             return cache_res.decode('utf-8')
 
-        # Fetch content and cache it
         output = method(*args, **kwargs)
-        red_loc.setex(cache_key, 10, output)  # Cache for 10 seconds
+        red_loc.setex(cache_key, 10, output)
         return output
 
     return wrapper
@@ -41,6 +38,6 @@ def get_page(url: str) -> str:
 
 
 if __name__ == '__main__':
-    url = 'http://slowwly.robertomurray.co.uk/'
+    url = 'http://slowwly.robertomurray.co.uk'
     for i in range(5):
         print(get_page(url))
