474d35a795bb10148bcbd957c1b0d904ddc8d5c3
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-10-24 13:05:46 +0100

    you

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,22 +8,20 @@ from typing import Callable
 red_loc = redis.Redis()
 
 
-def count_calls(method: Callable) -> Callable:
+def count_calls(method):
     """Decorator to count calls and implement caching"""
     @wraps(method)
-    def wrapper(*args, **kwargs):
+    def wrapper(url):
         """Wrap Function"""
-        url = args[0]
         count_key = f"count:{url}"
-        cache_key = f"cache:{url}"
-
-        red_loc.incr(count_key)
+        cache_key = f"cached:{url}"
 
         cache_res = red_loc.get(cache_key)
         if cache_res:
             return cache_res.decode('utf-8')
 
-        output = method(*args, **kwargs)
+        red_loc.incr(count_key)
+        output = method(url)
         red_loc.setex(cache_key, 10, output)
         return output
 
@@ -34,10 +32,4 @@ def count_calls(method: Callable) -> Callable:
 def get_page(url: str) -> str:
     """Fetch the content of a URL"""
     response = requests.get(url)
-    return response.text
-
-
-if __name__ == '__main__':
-    url = 'http://slowwly.robertomurray.co.uk'
-    for i in range(5):
-        print(get_page(url))
+    return response.text)
